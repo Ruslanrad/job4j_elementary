4d19661f580ae5d9b56aafd7e18008c626af539f
RuslanRad <r140390@yandex.ru>
2024-11-10 00:05:30 +0500

    1.1.7 / 6.8. Метод двух указателей - исправление [#243671]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -5,30 +5,25 @@ public class SimpleStringEncoder {
         String result = "";
         char symbol = input.charAt(0);
         int counter = 1;
-        if (input.length() == 1) {
-            result += symbol;
-        } else {
-            for (int i = 1; i < input.length(); i++) {
-                if (input.charAt(i) == symbol && i == input.length() - 1) {
-                    counter++;
-                    result += String.valueOf(symbol) + counter;
-                } else if (input.charAt(i) != symbol && i == input.length() - 1) {
-                    result += symbol;
-                    if (counter > 1) {
-                        result += counter;
-                    }
-                    result += input.charAt(i);
-                } else if (input.charAt(i) != symbol) {
-                    result += symbol;
-                    if (counter > 1) {
-                        result += counter;
-                    }
-                    counter = 1;
-                    symbol = input.charAt(i);
-                } else if (input.charAt(i) == symbol) {
-                    counter++;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.length() == 1) {
+                result += symbol;
+                return result;
+            } else if (input.charAt(i) == symbol && i > 0) {
+                counter++;
+            } else if (input.charAt(i) != symbol) {
+                result += symbol;
+                if (counter > 1) {
+                    result += counter;
                 }
+                counter = 1;
+                symbol = input.charAt(i);
             }
+        }
+
+        result += symbol;
+        if (counter > 1) {
+            result += counter;
         }
 
         return result;
